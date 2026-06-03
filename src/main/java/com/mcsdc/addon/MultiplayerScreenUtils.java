1cf3e826e77fcac6c21742e8876a9638bb29790b
Scripted <109150512+MaybeScripted@users.noreply.github.com>
2026-06-03 03:18:23 +0200

    server list helper so add/remove doesnt duplicate save reload crap

--- src/main/java/com/mcsdc/addon/MultiplayerScreenUtils.java
+++ src/main/java/com/mcsdc/addon/MultiplayerScreenUtils.java
@@ -3,18 +3,11 @@ package com.mcsdc.addon;
 import com.mcsdc.addon.mixin.MultiplayerScreenAccessor;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 
-public class MultiplayerScreenUtils {
-    public static void save(MultiplayerScreen multiplayerScreen) {
-        multiplayerScreen.getServerList().saveFile();
-    }
+public final class MultiplayerScreenUtils {
+    private MultiplayerScreenUtils() {}
 
-    public static void reload(MultiplayerScreen multiplayerScreen){
-        MultiplayerScreenAccessor msa = getAccessor(multiplayerScreen);
-        msa.getServerListWidget().setServers(multiplayerScreen.getServerList());
-    }
-
-
-    public static MultiplayerScreenAccessor getAccessor(MultiplayerScreen multiplayerScreen){
-        return (MultiplayerScreenAccessor) multiplayerScreen;
+    public static void reload(MultiplayerScreen multiplayerScreen) {
+        MultiplayerScreenAccessor accessor = (MultiplayerScreenAccessor) multiplayerScreen;
+        accessor.getServerListWidget().setServers(multiplayerScreen.getServerList());
     }
 }
